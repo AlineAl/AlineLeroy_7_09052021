ffd5cfe5ad70bea0b9482d077370309554302de6
Aline Leroy <aline.leroy.27@gmail.com>
2021-05-12 17:52:31 +0200

    installation express pour le server backend, création server.js et app.js, tests du localhost, création dossiers routes, models, controllers et design du schéma de la BDD

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+/* app.use((req, res, next) => {
+    res.json({message: "coucou"});
+    next();
+}) */
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.status(200);
+    next();
+})
+
+// app.use('/api/articles', articleRoutes);
+// app.use('/api/auth', userRoutes);
+
+module.exports = app;
