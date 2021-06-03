4f39f9dcb1419180bdb572a267760e9d6ddfd89b
Aline Leroy <aline.leroy.27@gmail.com>
2021-06-03 20:03:05 +0200

    installation de node sass et sass loader, affichage des articles avec axios

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Articles />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Articles from './components/Articles';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Articles
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './assets/style.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ frontend/src/components/Articles.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <table>
+            <tr v-for="item in list" v-bind:key="item.id">
+                <td>{{item.title}}</td>
+                <td>{{item.content}}</td>
+                <td>{{item.likes}}</td>
+                <td>{{item.User.firstname}}</td>
+                <td>{{item.User.lastname}}</td>
+            </tr>
+        </table>
+    </div> 
+</template>
+<script>
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios)
+    export default {
+        name: 'Articles',
+        data()
+        {
+            return {list: undefined}
+        },
+        mounted()
+        {
+            Vue.axios.get('http://localhost:3000/api/articles')
+            .then((response) => {
+                this.list = response.data
+                console.log(response);
+            })
+        }
+    }
+</script>
+<style lang="scss">
+
+</style>
