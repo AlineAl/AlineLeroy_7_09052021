2ee87306d5cde4968956f5440800c5a21fcd7e79
Aline Leroy <aline.leroy.27@gmail.com>
2021-06-05 18:06:23 +0200

    codage front de la navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <style lang="scss">
-@import './assets/style.css';
+@import './assets/scss/style.css';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- frontend/src/components/Articles.vue
+++ frontend/src/components/Articles.vue
@@ -1,14 +1,33 @@
 <template>
-    <div>
-        <table>
-            <tr v-for="item in list" v-bind:key="item.id">
-                <td>{{item.title}}</td>
-                <td>{{item.content}}</td>
-                <td>{{item.likes}}</td>
-                <td>{{item.User.firstname}}</td>
-                <td>{{item.User.lastname}}</td>
-            </tr>
-        </table>
+    <div class="container">
+        <div class="navbar">
+            <div>
+               <img src="../assets/images/icon-left-font-monochrome-black.png" alt="groupomania" class="logo"> 
+            </div>
+            <div class="list">
+                <div>
+                   <ul>
+                        <li class='list-navbar'><i class="fas fa-bell"></i></li>
+                        <li class='list-navbar'><i class="fas fa-list"></i></li>
+                    </ul> 
+                </div>
+                <div>
+                   <img src="../assets/images/—Pngtree—vector users icon_4144740.png" alt="photo de profil" class="avatar"> 
+                </div>
+            </div>
+        </div>
+        <div>
+           <table>
+                <tr v-for="item in list" v-bind:key="item.id">
+                    <td>{{item.title}}</td>
+                    <td>{{item.content}}</td>
+                    <td>{{item.likes}}</td>
+                    <td>{{item.User.firstname}}</td>
+                    <td>{{item.User.lastname}}</td>
+                </tr>
+            </table> 
+        </div>
+        
     </div> 
 </template>
 <script>
